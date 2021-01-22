2eca4d6169f7be518a12bb9389eb44f49bb19155
corycalaway <cory.c.calaway@gmail.com>
2021-01-22 09:41:41 -0700

    first pasing test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,13 +2,14 @@ const { test, expect } = require('@jest/globals')
 const Employee = require('../lib/Employee')
 
 test('creates and employee object', () => {
-    const employee = new Employee('Cory');
+    const employee = new Employee('Cory', 1234, '1234@gmail');
 
     expect(employee.name).toBe('Cory');
     expect(employee.id).toEqual(expect.any(Number));
-
+    expect(employee.email).toEqual(expect.stringContaining('@'))
     //how to check if email in jest
  //   expect(employee.email).toEqual(expect.any(Number));
     
     
 });
+
