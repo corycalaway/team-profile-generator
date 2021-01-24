5b3a4fd75fef4be363ef321f427ba7d76e4cf022
Cory Calaway <cory.c.calaway@gmail.com>
2021-01-24 03:43:46 -0700

    able to gather infromation from all sections

--- Index.js
+++ Index.js
@@ -4,22 +4,27 @@ const managerInfo = require('./lib/Manager')
 // const internInfo = require('./Intern')
 // const engineerInfo = require('./Engineer')
 const employeeInfo = require('./lib/Employee')
+const managerInfoData = require('./lib/Manager')
 
-
-new questionsManager().startManager()
+new questionsManager().questionsManagerData()
 // .then( managerComplete => {
 //     return new managerInfo(managerComplete)
 
 
-    .then(managerReturn => {
+    // .then(employeeReturn => {
                       
-        const {employeeName, employeeId, employeeEmail, officeNumber} = managerReturn
-        new employeeInfo(employeeName, employeeId, employeeEmail)
-        new managerInfo(officeNumber)
+    //     const {employeeName, employeeId, employeeEmail} = employeeReturn
+    //     new employeeInfo(employeeName, employeeId, employeeEmail)
+    //     // new managerInfo(officeNumber)
         
      
-    })
+    // })
+    // .then(managerReturn => {
+    //     return new questionsManager().managerPrompt(managerReturn)
+    // }).then(managerSave => {
+    //     new managerInfoData(managerSave)
+    // })
 // })
-.then( choices => {
-   new questionsManager().choicesPrompt(choices)
-})
+// .then( choices => {
+//    new questionsManager().choicesPrompt(choices)
+// })
