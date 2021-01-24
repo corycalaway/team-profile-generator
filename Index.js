1e33f8eb6890df2e5d5d17c321eb1f66b81fafd6
Cory Calaway <cory.c.calaway@gmail.com>
2021-01-23 23:48:45 -0700

    adjusted employee into a class

--- Index.js
+++ Index.js
@@ -1,3 +1,14 @@
 const questionsManager = require('./lib/Employee');
+// const inquirer = require('inquirer');
+const managerInfo = require('./lib/Manager')
+// const internInfo = require('./Intern')
+// const engineerInfo = require('./Engineer')
 
-new questionsManager().startManager();
+
+new questionsManager().startManager()
+.then( managerComplete => {
+    return new managerInfo(managerComplete)
+})
+.then( choices => {
+   new questionsManager().choicesPrompt(choices)
+})
