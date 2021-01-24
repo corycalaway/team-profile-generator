21f0a3f763664889ea2b54f8771e98efe9df871e
Cory Calaway <cory.c.calaway@gmail.com>
2021-01-24 01:02:42 -0700

    fixed classes for intern manager and employee

--- Index.js
+++ Index.js
@@ -1,14 +1,25 @@
-const questionsManager = require('./lib/Employee');
+const questionsManager = require('./lib/EmployeeLaunch');
 // const inquirer = require('inquirer');
 const managerInfo = require('./lib/Manager')
 // const internInfo = require('./Intern')
 // const engineerInfo = require('./Engineer')
+const employeeInfo = require('./lib/Employee')
 
 
 new questionsManager().startManager()
-.then( managerComplete => {
-    return new managerInfo(managerComplete)
-})
+// .then( managerComplete => {
+//     return new managerInfo(managerComplete)
+
+
+    .then(managerReturn => {
+                      
+        const {employeeName, employeeId, employeeEmail, officeNumber} = managerReturn
+        new employeeInfo(employeeName, employeeId, employeeEmail)
+        new managerInfo(officeNumber)
+        
+     
+    })
+// })
 .then( choices => {
    new questionsManager().choicesPrompt(choices)
 })
--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,12 +1,14 @@
 const { test, expect } = require('@jest/globals')
 const questionsManager = require('../lib/Employee')
+const Manager = require('../lib/Manager')
 
 test('creates and employee object', () => {
-    const employee = new questionsManager('Cory');
+    const employee = new Manager('Cory', 1234, '1234@gmail', 12);
 
-    expect(employee.employeeName).toBe('Cory');
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toEqual(expect.stringContaining('@'))
+    expect(employee.managerName).toBe('Cory');
+    expect(employee.managerId).toEqual(expect.any(Number));
+    expect(employee.managerEmail).toEqual(expect.stringContaining('@'))
+    expect(employee.officeNumber).toBeGreaterThanOrEqual(1);
 
     // this.employeeName;
     // this.employeeId;
