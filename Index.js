3866d720684e17f9514a447c641303bbfa084201
Cory Calaway <cory.c.calaway@gmail.com>
2021-01-25 14:51:53 -0700

    layout created and starting initial questions

--- Index.js
+++ Index.js
@@ -1,9 +1,64 @@
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-const employeeInfo = require('./lib/Employee')
+class Aplication {
+  constructor(managerData) {
+    this.manager;
+    this.managerData = [];
+  }
 
+  startApp() {
+    this.manager = new Employee();
 
-let employeeDataInfo = new employeeInfo()
+    inquirer
+      .prompt({
+        type: "text",
+        name: "employeeName",
+        message: "Please enter Managers name",
+      })
+      .then(({ employeeName }) => {
+        this.manager = new Employee(employeeName);
+        console.log(employeeName)
 
-employeeDataInfo.questionsManagerData()
+        this.managerData.push(employeeName)
+        console.log(this.managerData)
+        this.employeeIdQuestion
+      });
+  }
 
-// 
+  getEmployeeName() {
+    // this.managerName.push(new Employee().getEmployeeName())
+    // // .then(
+    // .then(() => {
+    //     // this.managerData.push(managerName)
+    // console.log(this.managerName)
+    // this.employeeIdQuestion()
+  }
+
+  employeeNameQuestion() {}
+
+  employeeIdQuestion() {
+    
+    this.managerId = new Employee().getEmployeeId();
+  }
+}
+
+// function structureApp() {
+//     this.managerData = [];
+
+// }
+// sets manager Info to equal employeeName
+new Aplication().startApp();
+
+// .then(savedData => {
+//     console.log(savedData)
+
+// })
+
+// }
+
+// structureApp();
