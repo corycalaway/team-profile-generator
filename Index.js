4fbe8d47a4792c38f18582518c7f681efadb86e1
Cory Calaway <cory.c.calaway@gmail.com>
2021-01-24 17:59:31 -0700

    restructure

--- Index.js
+++ Index.js
@@ -2,25 +2,6 @@
 const employeeInfo = require('./lib/Employee')
 
 
-new employeeInfo().questionsManagerData()
-// .then( managerComplete => {
-//     return new managerInfo(managerComplete)
+let employeeDataInfo = new employeeInfo()
 
-
-    // .then(employeeReturn => {
-                      
-    //     const {employeeName, employeeId, employeeEmail} = employeeReturn
-    //     new employeeInfo(employeeName, employeeId, employeeEmail)
-    //     // new managerInfo(officeNumber)
-        
-     
-    // })
-    // .then(managerReturn => {
-    //     return new questionsManager().managerPrompt(managerReturn)
-    // }).then(managerSave => {
-    //     new managerInfoData(managerSave)
-    // })
-// })
-// .then( choices => {
-//    new questionsManager().choicesPrompt(choices)
-// })
+employeeDataInfo.questionsManagerData()
