e34e0a6e7c9cc0df00bff7c76389c32927ac6b75
Cory Calaway <cory.c.calaway@gmail.com>
2021-01-26 13:50:55 -0700

    added bootstrap to html

--- Index.js
+++ Index.js
@@ -231,7 +231,22 @@ class Aplication {
     console.log(this.employeeData)
 
     console.log(this.completedData)
-
+    this.completedData = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="./assets/css/style.css" rel="stylesheet" />
+    
+    </head>
+    <body>
+    
+        
+    </body>
+    </html>`
 
    this.createHtml(this.completedData)
     // this.generateData()
